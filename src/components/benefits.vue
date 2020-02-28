2981fc0a9f088478fdf236e482f6ab200a1e706b
Ian <ianpc122@gmail.com>
2020-02-28 14:25:18 +0000

    added benefits

---
+++ src/components/benefits.vue
@@ -0,0 +1,38 @@
+<template lang="html">
+  <div>
+    <h1>The Benefits of yoga.</h1>
+    <h3> Improve hip movement </h3>
+    <p>Hip hinge movement is the pattern
+      of flexion and extension throughout
+      the hip joint. Especially important for
+      runners, hikers and people who enjoy kettle
+      bell workouts, proper hip hinge movement lessens
+      the chance of athletic injury by decreasing
+      stress on the joint. Because yoga places such
+      a stringent focus on knee, ankle, hip and sacrum
+      alignment during static poses, it helps to strengthen
+      and loosen the hip joints while also promoting healthy hip
+      hinge movement that carries over into many activities.</p>
+      <h3>Improves bones and Muscles</h3>
+      <p>Practicing yoga on a regular basis improves the health
+        of your bones while strengthening your spine and muscles.
+        It also works the tendons and ligaments, making them less
+        susceptible to breakdown. And, of course, yoga improves your
+        flexibility. These yoga benefits work together to protect your
+        body from injury.</p>
+        <h3>  Protects your spine</h3>
+        <p> Spinal disks—the shock
+          absorbers between the vertebrae that can herniate and compress nerves—crave
+          movement. That's the only way they get their nutrients. If you've got a
+          well-balanced asana practice with plenty of backbends, forward bends, and twists,
+          you'll help keep your disks supple.</p>
+        </div>
+      </template>
+
+      <script>
+      export default {
+      }
+      </script>
+
+      <style lang="css" scoped>
+      </style>
