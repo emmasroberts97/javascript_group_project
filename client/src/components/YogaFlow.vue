803d4838eff898b0d7fa3a0f89107e73d203a285
Emma Roberts <emma.roberts197@gmail.com>
2020-02-29 10:21:54 +0000

    Added slideshow

--- client/src/components/YogaFlow.vue
+++ client/src/components/YogaFlow.vue
@@ -1,11 +1,25 @@
 <template lang="html">
-
+<div>
+  <vueper-slides autoplay duration="10000">
+    <vueper-slide v-for="(pose, index) in flow"
+      :key="pose.id"
+      :image="pose.img_url" />
+    <template v-slot:pause>
+      <i class="icon pause_circle_outline"></i>
+    </template>
+  </vueper-slides>
+</div>
 </template>
 
 <script>
+
+import { VueperSlides, VueperSlide } from 'vueperslides'
+import 'vueperslides/dist/vueperslides.css'
+
 export default {
   name: "yoga-flow",
-  props: ['flow']
+  props: ['flow'],
+  components: { VueperSlides, VueperSlide }
 }
 </script>
 
