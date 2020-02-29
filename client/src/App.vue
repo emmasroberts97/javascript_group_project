a47e1d0d4290370a97735048eb078b464a153e3d
Emma Roberts <emma.roberts197@gmail.com>
2020-02-29 09:22:14 +0000

    edited files

---
+++ client/src/App.vue
@@ -0,0 +1,31 @@
+<template lang="html">
+<div>
+  <div class="">
+    <sequence-builder />
+    <history/>
+  </div>
+</div>
+</template>
+
+<script>
+import SequenceBuilder from './components/SequenceBuilder.vue';
+import History from './components/History.vue';
+import YogaTypes from './components/YogaTypes.vue';
+
+export default {
+  name: 'app',
+  data() {
+    return {
+      selectedTab: ""
+    }
+  },
+  components: {
+    "sequence-builder": SequenceBuilder,
+    "history": History,
+    "yoga-types": YogaTypes
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
---
+++ client/src/components/SequenceBuilder.vue
@@ -0,0 +1,32 @@
+<template lang="html">
+<div>
+<yoga-grid :yogaPoses="yogaPoses" />
+</div>
+</template>
+
+<script>
+
+import YogaGrid from "./YogaGrid.vue";
+
+export default {
+  name: "sequence-builder",
+  data() {
+    return {
+      yogaPoses: []
+    }
+  },
+  mounted() {
+      fetch('https://raw.githubusercontent.com/rebeccaestes/yoga_api/master/yoga_api.json')
+      .then(res => res.json())
+      .then(data => {
+        this.yogaPoses = data;
+      })
+  },
+  components: {
+    "yoga-grid": YogaGrid
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
