4ce48a28bd21a0d2fba779b48dfe636b5fe2a38b
Emma Roberts <emma.roberts197@gmail.com>
2020-03-02 14:14:57 +0000

    fixed slideshow bug

--- client/src/components/YogaFlow.vue
+++ client/src/components/YogaFlow.vue
@@ -1,10 +1,10 @@
 <template lang="html">
 <article>
 <h1>Your Yoga Flow</h1>
-  <vueper-slides autoplay duration="10000" :slide-ratio="400/400" @slide="playSlide($event)" @ready="playSlide($event)" slideContentOutside='true'>
+  <vueper-slides autoplay duration="10000" :slide-ratio="400/400" @ready="playSlide($event)" @slide="playSlide($event)" slideContentOutside='true'>
     <vueper-slide v-for="(pose, index) in flow"
       :title="pose.english_name"
-      :key="pose.id"
+      :key="index"
       :image="pose.img_url" />
     <template v-slot:pause>
       <i class="icon pause_circle_outline"></i>
@@ -26,13 +26,12 @@ export default {
   },
   methods: {
     playSlide: function(slide) {
-      console.log(slide)
         let msg = new SpeechSynthesisUtterance();
         let voices = window.speechSynthesis.getVoices();
         msg.voice = voices[10];
         msg.voiceURI = "native";
         msg.volume = 3;
-        msg.rate = 0.5;
+        msg.rate = 0.8;
         msg.pitch = 0.8;
         msg.text = "Let's get into:" + slide.currentSlide.title;
         msg.lang = 'en-US';
