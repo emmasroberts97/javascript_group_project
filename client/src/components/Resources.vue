080467f75ec4fd37ea7f5ee830cab533fab75c10
Ian <ianpc122@gmail.com>
2020-03-03 14:37:45 +0000

    database updated and name added

--- client/src/components/Resources.vue
+++ client/src/components/Resources.vue
@@ -19,14 +19,7 @@
     :url="url"
     :attribution="attribution"
     />
-    <!-- <l-marker :lat-lng="withPopup">
-    <l-popup>
-    <div @click="innerClick">
-    I am a popup
-    <p v-show="showParagraph">  </p>
-  </div>
-</l-popup>
-</l-marker> -->
+
 <l-marker :lat-lng="withTooltip">
   <l-tooltip :options="{ permanent: true, interactive: true }">
 
@@ -40,66 +33,7 @@
 </l-marker>
 
 <map-marker v-for="(location, index) in yoga" :key="index" :location="location"/>
-<!-- <div class="yoga-studio-marker" v-for="(location, index) in yoga" :key="index" @click="showInfo">
-  <l-marker :lat-lng="getLatlng(location.latLng)">
-    <l-tooltip :options="{ permanent: true, interactive: true}">
-      <img v-on:mouseover="showParagraph = true" @mouseleave="showParagraph = false" :set="showDeets = showParagraph" src="../assets/mat.png" height="20px">
-      <div v-show="showDeets">
-        <h1>{{location.name}}</h1>
-      </div>
-    </l-tooltip>
-  </l-marker>
-</div> -->
-<!-- <l-marker :lat-lng="HopeStudioYoga">
-<l-tooltip :options="{ permanent: true, interactive: true}">
-<div @click="innerClick">
-Hope Studio Yoga
-<p v-show="showParagraph">
-5th Floor, Central Chambers, 404, 93 Hope St Glasgow G2 6LD
-</p>
-</div>
-</l-tooltip>
-</l-marker>
-<l-marker :lat-lng="MerchantCityYoga">
-<l-tooltip :options="{ permanent: true, interactive: true}">
-<div @click="innerClick">
-Merchant City Yoga
-<p v-show="showParagraph">
-2, 49 Virgina St, Glasgow G1 1TS
-</p>
-</div>
-</l-tooltip>
-</l-marker>
-<l-marker :lat-lng="TheYogaExtension">
-<l-tooltip :options="{ permanent: true, interactive: true}">
-<div @click="innerClick">
-The Yoga extension
-<p v-show="showParagraph">
-Suite A, 5 Newton Terrace Ln, Glasgow G3 7PB
-</p>
-</div>
-</l-tooltip>
-</l-marker>
-<l-marker :lat-lng="InfintityYoga">
-<l-tooltip :options="{ permanent: true, interactive: true}">
-<div @click="innerClick">
-Infintity Yoga
-<p v-show="showParagraph">
-11 Osborne St, Glasgow G1 5QN
-</p>
-</div>
-</l-tooltip>
-</l-marker>
-<l-marker :lat-lng="MoveStudios">
-<l-tooltip :options="{ permanent: true, interactive: true}">
-<div @click="innerClick">
-Move Studios
-<p v-show="showParagraph">
-27-45 Stockwell Pl, Glasgow G1 4HS
-</p>
-</div>
-</l-tooltip>
-</l-marker> -->
+
 </l-map>
 </div>
 </div>
@@ -130,11 +64,6 @@ export default {
       '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
       withPopup: latLng(55.865505, -4.2592),
       withTooltip: latLng(55.865505, -4.2592),
-      HopeStudioYoga: latLng(55.861138, -4.259079),
-      MerchantCityYoga: latLng(55.859569, -4.250090),
-      TheYogaExtension: latLng(55.865791, -4.274662),
-      InfintityYoga: latLng(55.855893, -4.245787),
-      MoveStudios: latLng(55.856735, -4.250980),
       currentZoom: 11.5,
       currentCenter: latLng(55.865505, -4.2592),
       showParagraph: false,
--- server/db/seeds.js
+++ server/db/seeds.js
@@ -31,5 +31,58 @@ db.manage.insertMany([
    name: "Shanti Yoga Glasgow",
    location: "20 Sandyford St, Yorkhill St, Glasgow G3 8QJ",
    latLng: [55.865124, -4.296059]
+ },
+ {
+   name: "The Kali Collective Yoga Studio",
+   location: "Suite 415B The Pentagon Building, 36 Washington St, Glasgow G3 8AZ",
+   latLng: [55.858441, -4.269150]
+ },
+ {
+   name: "Yoga Healing Glasgow",
+   location: "Studio 7, 22 Mansfield St, Partick, Glasgow G11 5QP",
+   latLng:[55.873220, -4.301544]
+ },
+ {
+   name:"The Movement Studio",
+   location: "4 Ashton Ln, Hillhead, Glasgow G12 8SJ",
+   latLng:[55.875504, -4.293593]
+ },
+ {
+   name:"Bikram Yoga Glasgow",
+   location:"1 Dowanside Ln, Glasgow G12 9BZ",
+   latLng:[55.875231, -4.294494]
+ },
+ {
+   name:"Our World Yoga",
+   location:"1000 Great Western Rd, Glasgow G12 0NR",
+   latLng:[55.884465, -4.306332]
+ },
+ {
+   name:"Hotpod Yoga Glasgow",
+   location:"Axiom Art, Studio 1-3, 54 Washington St, Glasgow G3 8AZ",
+   latLng:[55.858518, -4.269272]
+ },
+ {
+   name:"Finnieston Yoga Shala",
+   location:"Yellow Building, 1103 Argyle Street The Hidden Lane, Glasgow G3 8ND",
+   latLng:[55.865348, -4.283528]
+ },
+ {
+   name:"The Yoga & Pilates Place & Injury Clinic Glasgow",
+   location:"32 Portman St, Glasgow G41 1JB",
+   latLng:[55.853173, -4.281506]
+ },
+ {
+   name:"Studio 70",
+   location:"70 Victoria Rd, Glasgow G42 7AA",
+   latLng:[55.841354, -4.263390]
+ },
+ {
+   name:"Gentle yoga classes",
+   location:"72 Berkeley St, Glasgow G3 7DS",
+   latLng:[55.865440, -4.275192]
+ },
+ {
+   name:""
  }
 ])
